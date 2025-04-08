0aafac8ee19b82baa0a6c8f7d723737cd2dbcf1c
PeichenY <pyao3@ucsc.edu>
2025-04-07 22:30:23 -0700

    upload

---
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    private float speed = 5.0f;
+    private float turnSpeed = 50.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // Move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        transform.Rotate(Vector3.up,  turnSpeed * horizontalInput * Time.deltaTime);
+        
+    }
+}
